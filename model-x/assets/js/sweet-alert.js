e4282909c00dc36cb97a9f1ed1ffe8a44144ad65
HyPerKung999 <lsxnsukh@gmail.com>
2024-09-16 23:31:05 +0700

    Update sweet-alert.js

--- model-x/assets/js/sweet-alert.js
+++ model-x/assets/js/sweet-alert.js
@@ -8,6 +8,7 @@ function AlertSend({
   cancelButtonColor,
   confirmButtonText,
   cancelButtonText,
+  showConfirmButton,
   showCancelButton
 }) {
   return new Promise((resolve) => {
@@ -15,46 +16,25 @@ function AlertSend({
     if (icon == null) icon = "info";
     if (title == null) title = "";
     if (text == null) text = "";
+    if (showConfirmButton == null) showConfirmButton = false;
     if (showCancelButton == null) showCancelButton = false;
     if (confirmButtonColor == null) confirmButtonColor = "#3085d6";
     if (cancelButtonColor == null) cancelButtonColor = "#d33";
     if (confirmButtonText == null) confirmButtonText = "ตกลง";
     if (cancelButtonText == null) cancelButtonText = "ยกเลิก";
 
-    if (icon == "question") {
-      Swal.fire({
-        icon: icon,
-        title: title,
-        html: text,
-        showCancelButton: showCancelButton,
-        confirmButtonColor: confirmButtonColor,
-        cancelButtonColor: cancelButtonColor,
-        confirmButtonText: confirmButtonText,
-        cancelButtonText: cancelButtonText
-      }).then((result) => {
-        resolve({ isConfirmed: result.isConfirmed });
-      })
-    } else {
-      if (confirmButtonText) {
-        Swal.fire({
-          icon: icon,
-          title: title,
-          html: text,
-          showCancelButton: showCancelButton,
-          confirmButtonColor: confirmButtonColor,
-          confirmButtonText: confirmButtonText,
-        }).then((result) => {
-          resolve({ isConfirmed: result.isConfirmed });
-        })
-      } else {
-        Swal.fire({
-          icon: icon,
-          title: title,
-          html: text,
-        }).then((result) => {
-          resolve({ isConfirmed: result.isConfirmed });
-        })
-      }
-    }
+    Swal.fire({
+      icon: icon,
+      title: title,
+      html: text,
+      showConfirmButton: showConfirmButton,
+      showCancelButton: showCancelButton,
+      confirmButtonColor: confirmButtonColor,
+      cancelButtonColor: cancelButtonColor,
+      confirmButtonText: confirmButtonText,
+      cancelButtonText: cancelButtonText
+    }).then((result) => {
+      resolve({ isConfirmed: result.isConfirmed });
+    })
   })
 }
