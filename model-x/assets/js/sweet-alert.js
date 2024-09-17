1fdc4acf2a9a2bbf4e08dc9e825982c545eb2a91
HyPerKung999 <lsxnsukh@gmail.com>
2024-09-17 12:18:42 +0700

    Update sweet-alert.js

--- model-x/assets/js/sweet-alert.js
+++ model-x/assets/js/sweet-alert.js
@@ -4,11 +4,8 @@ function AlertSend({
   icon,
   title,
   text,
-  confirmButtonColor,
-  cancelButtonColor,
   confirmButtonText,
   cancelButtonText,
-  showConfirmButton,
   showCancelButton
 }) {
   return new Promise((resolve) => {
@@ -16,25 +13,43 @@ function AlertSend({
     if (icon == null) icon = "info";
     if (title == null) title = "";
     if (text == null) text = "";
-    if (showConfirmButton == null) showConfirmButton = false;
-    if (showCancelButton == null) showCancelButton = false;
-    if (confirmButtonColor == null) confirmButtonColor = "#3085d6";
-    if (cancelButtonColor == null) cancelButtonColor = "#d33";
-    if (confirmButtonText == null) confirmButtonText = "ตกลง";
     if (cancelButtonText == null) cancelButtonText = "ยกเลิก";
+    if (showCancelButton == null) showCancelButton = false;
 
-    Swal.fire({
-      icon: icon,
-      title: title,
-      html: text,
-      showConfirmButton: showConfirmButton,
-      showCancelButton: showCancelButton,
-      confirmButtonColor: confirmButtonColor,
-      cancelButtonColor: cancelButtonColor,
-      confirmButtonText: confirmButtonText,
-      cancelButtonText: cancelButtonText
-    }).then((result) => {
-      resolve({ isConfirmed: result.isConfirmed });
-    })
+    if (icon == "question") {
+      Swal.fire({
+        icon: icon,
+        title: title,
+        html: text,
+        showCancelButton: showCancelButton,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: confirmButtonText,
+        cancelButtonText: cancelButtonText
+      }).then((result) => {
+        resolve({ isConfirmed: result.isConfirmed });
+      })
+    } else {
+      if (confirmButtonText) {
+        Swal.fire({
+          icon: icon,
+          title: title,
+          html: text,
+          showCancelButton: showCancelButton,
+          confirmButtonColor: '#3085d6',
+          confirmButtonText: confirmButtonText,
+        }).then((result) => {
+          resolve({ isConfirmed: result.isConfirmed });
+        })
+      } else {
+        Swal.fire({
+          icon: icon,
+          title: title,
+          html: text,
+        }).then((result) => {
+          resolve({ isConfirmed: result.isConfirmed });
+        })
+      }
+    }
   })
 }
